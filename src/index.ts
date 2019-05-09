ea6b64e84cc6ba3fd66335c198f7c48cc8c0e56c
teramotodaiki <i101323@gmail.com>
2019-05-09 22:48:38 +0900

    chore: requirejs test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-const div = document.createElement("div");
-div.textContent = "Hello Sandbox!";
+const div = document.createElement('div');
+// div.textContent = "Hello Sandbox!";
 
-document.querySelector("body").appendChild(div);
+const value = sessionStorage.getItem('value') + '~';
+div.textContent = value;
+sessionStorage.setItem('value', value);
+
+document.body.appendChild(div);
+
+const { requirejs, define } = require('./require');
+
+define('hogehoge', function(require: any, exports: any, module: any) {
+  module.exports = 'hogehoge';
+});
+
+define('main', function(require: any, exports: any, module: any) {
+  const hogehoge = require('hogehoge');
+  console.log(hogehoge);
+  module.exports = hogehoge + 'fugafuga';
+});
+
+requirejs(['main'], (main: any) => console.log('main', main));

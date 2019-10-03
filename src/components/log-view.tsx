605b21ab2c973ce3534c091d6bfcbdc92f27d8dc
teramotodaiki <i101323@gmail.com>
2019-10-04 01:34:36 +0900

    fix: filter query

--- src/components/log-view.tsx
+++ src/components/log-view.tsx
@@ -1,13 +1,25 @@
 import { createLogger, ILogBody } from '@hackforplay/log';
 import * as React from 'react';
 
-type R = React.Reducer<number, void>;
+type R<T, A> = React.Reducer<T, A>;
 
 export function LogView() {
   const loggerRef = React.useRef(createLogger());
-  const [, forceUpdate] = React.useReducer<R>(i => i + 1, 0);
+  const [, forceUpdate] = React.useReducer<R<number, void>>(i => i + 1, 0);
+  const [expanded, toggle] = React.useReducer<R<boolean, any>>(b => !b, false);
+  const [query, _setQuery] = React.useState('');
+  const setQuery = React.useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => _setQuery(e.currentTarget.value),
+    []
+  );
+  const ignore = React.useCallback(
+    (e: React.BaseSyntheticEvent) => e.stopPropagation(),
+    []
+  );
 
-  const logs = loggerRef.current.logs.slice(-1000);
+  const logs = loggerRef.current.logs
+    .filter(log => (log.line[1] || '').includes(query))
+    .slice(-1000);
   const [first] = loggerRef.current.logs;
   const offsetTime = (first && first.time) || 0;
 
@@ -23,12 +35,39 @@ export function LogView() {
         bottom: 0,
         backgroundColor: 'rgba(0,0,0,0.5)',
         color: 'rgb(255,255,255)',
-        width: '100%'
+        width: '100%',
+        fontSize: expanded ? '1rem' : '0.75rem'
       }}
     >
-      {logs.map((log, i) => (
-        <LogItem key={i} log={log} offsetTime={offsetTime} />
-      ))}
+      {expanded ? (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+            backgroundColor: '#D8D8D8',
+            cursor: 'pointer'
+          }}
+          onClick={toggle}
+        >
+          <input
+            type="text"
+            placeholder="Filter"
+            value={query}
+            onChange={setQuery}
+            onClick={ignore}
+            autoFocus
+          />
+        </div>
+      ) : null}
+      {!expanded && first ? (
+        <LogItem log={first} offsetTime={offsetTime} onClick={toggle} />
+      ) : null}
+      {expanded
+        ? logs.map((log, i) => (
+            <LogItem key={i} log={log} offsetTime={offsetTime} />
+          ))
+        : null}
     </div>
   );
 }
@@ -36,6 +75,7 @@ export function LogView() {
 export interface LogItemProps {
   log: ILogBody;
   offsetTime: number;
+  onClick?: (value: any) => void; // hack for allow react.Dispatch
 }
 
 const iconColors: { [type: string]: string | undefined } = {
@@ -46,7 +86,7 @@ const iconColors: { [type: string]: string | undefined } = {
   error: '#E47070'
 };
 
-export function LogItem({ log, offsetTime }: LogItemProps) {
+export function LogItem({ log, offsetTime, onClick }: LogItemProps) {
   const [type, message] = log.line;
   const time = (log.time - offsetTime) / 1000;
   const color = iconColors[type] || 'white';
@@ -56,8 +96,10 @@ export function LogItem({ log, offsetTime }: LogItemProps) {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        cursor: onClick && 'pointer'
       }}
+      onClick={onClick}
     >
       <span>{(time.toFixed(1) + '').padStart(5, '0')}</span>
       <span
